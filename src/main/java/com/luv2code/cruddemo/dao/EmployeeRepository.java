27a9c25bf8a7dbc28bb508884fe3c5970d92ad7b
ishumehta <mehta.ishu14@gmail.com>
2025-06-16 16:05:00 +0530

    alpha test

--- src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 //    that's it, no need to implement any methods ourselves
-
+// checking git updates
 }
