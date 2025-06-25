ef8b3bb9f04f91f5936af75e8df9c79b2efa8ad5
ishumehta <mehta.ishu14@gmail.com>
2025-06-25 10:13:13 +0530

    added spring security with credentials

--- src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
@@ -4,7 +4,7 @@ import com.luv2code.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
+@RepositoryRestResource(path="employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 //    that's it, no need to implement any methods ourselves
